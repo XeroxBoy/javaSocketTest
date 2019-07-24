f539e74aaa13ea9bb662bfd85462c59f1417b05f
AlexAnderIch <827312773@qq.com>
2019-07-24 16:36:08 +0800

    java socket thread test

--- src/ClientSocket/ClientSocket.java
+++ src/ClientSocket/ClientSocket.java
@@ -1,51 +1,49 @@
 package ClientSocket;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class ClientSocket {
     public static void main(String args[]) throws Exception {
         // 要连接的服务端IP地址和端口
         String host = "127.0.0.1";
         int port = 55533;
-        ExecutorService threadPool = Executors.newFixedThreadPool(100);
         // 与服务端建立连接
         Socket socket = new Socket(host, port);
         // 建立连接后获得输出流
-        OutputStream outputStream = socket.getOutputStream();
-
-            Runnable runnable = () -> {
-                String message = "你好  yiwangzhibujian";
+        OutputStream outputStream = null;
+        InputStream inputStream = null;
                 try {
-                    outputStream.write(message.getBytes(StandardCharsets.UTF_8));
+                    outputStream = socket.getOutputStream();
+                    outputStream.write(("我是客户端" + Math.random() * 100 + "号").getBytes(StandardCharsets.UTF_8));
+                    socket.shutdownOutput();
+                    inputStream = socket.getInputStream();
+                    byte[] bytes = new byte[1024];
+                    StringBuilder sb = new StringBuilder();
+                    int len;
+                    while ((len = inputStream.read(bytes)) != -1) {
+                        sb.append(new String(bytes, 0, len, StandardCharsets.UTF_8));
+                    }
+                    System.out.println("客户端收到了" + sb);
                 } catch (IOException e) {
                     e.printStackTrace();
+                } finally {
+                    if (outputStream != null) {
+                        outputStream.close();
+                    }
+                    if (inputStream != null) {
+                        inputStream.close();
+                    }
+                    if (socket != null) {
+                        socket.close();
+                    }
                 }
-            };
-
-        threadPool.submit(runnable);
-            outputStream.close();
-            socket.close();
-
-        //通过shutdownOutput高速服务器已经发送完数据，后续只能接受数据
-//        socket.shutdownOutput();
-
-//        InputStream inputStream = socket.getInputStream();
-//        byte[] bytes = new byte[1024];
-//        int len;
-//        StringBuilder sb = new StringBuilder();
-//        while ((len = inputStream.read(bytes)) != -1) {
-//            //注意指定编码格式，发送方和接收方一定要统一，建议使用UTF-8
-//            sb.append(new String(bytes, 0, len, StandardCharsets.UTF_8));
-//        }
-//        System.out.println("get message from server: " + sb);
-//        inputStream.close();
-        }
     }
+}
+
 
 
 
--- src/ServerSocket/serverThread.java
+++ src/ServerSocket/serverThread.java
@@ -1,19 +1,54 @@
 package ServerSocket;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 
 public class serverThread implements Runnable{
     Socket socket = null;
     InetAddress inetAddress = null;
 
-    public serverThread(Socket socket, InetAddress inetAddress) {
+    serverThread(Socket socket, InetAddress inetAddress) {
         this.socket = socket;
         this.inetAddress = inetAddress;
     }
 
     @Override
     public void run() {
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
+        try {
+            inputStream = socket.getInputStream();
+            byte[] bytes = new byte[1024];
+            int len;
+            StringBuilder sb = new StringBuilder();
+            while ((len = inputStream.read(bytes)) != -1) {
+                sb.append(new String(bytes, 0, len, StandardCharsets.UTF_8));
+            }
+            System.out.println("客户端说：" + sb);
+            outputStream = socket.getOutputStream();
+            outputStream.write("服务器已收到消息，内容为:".getBytes(StandardCharsets.UTF_8));
+            outputStream.write(sb.toString().getBytes(StandardCharsets.UTF_8));
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+                if (socket != null) {
+                    socket.close();
+                }
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
 
     }
 }
